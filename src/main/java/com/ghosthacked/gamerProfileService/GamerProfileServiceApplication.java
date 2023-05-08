97da589bb564d5622d535258f99c1cec3f5ac5fe
Connor Kendrick <mail@connor.codes>
2023-05-08 01:34:17 -0700

    Add project files

---
+++ src/main/java/com/ghosthacked/gamerProfileService/GamerProfileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ghosthacked.gamerProfileService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerProfileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerProfileServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghosthacked/gamerProfileService/GamerProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghosthacked.gamerProfileService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamerProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
